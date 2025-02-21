65d5e3f53844c26682269bb5c6bf86ddf7f1a385
Matt Carey <matt@stackone.com>
2025-02-21 15:07:15 +0000

    feat: stackone ids

---
+++ examples/stackone_account_ids.py
@@ -0,0 +1,38 @@
+"""
+Handling StackOne account IDs with the StackOne Tools.
+
+```bash
+uv run examples/stackone_account_ids.py
+```
+"""
+
+import os
+
+from dotenv import load_dotenv
+from stackone_ai import StackOneToolSet
+
+load_dotenv()
+
+if not os.getenv("STACKONE_API_KEY"):
+    raise ValueError("Please set STACKONE_API_KEY in your environment or a .env file")
+
+
+def main():
+    toolset = StackOneToolSet()
+
+    # Filter by vertical and set the account ID
+    tools = toolset.get_tools(vertical="hris", account_id="test_id")
+
+    # You can over write the account ID here..
+    tools.set_account_id("a_different_id")
+
+    employee_tool = tools.get_tool("get_employee")
+    if employee_tool:
+        # You can even set the account ID on a per-tool basis
+        employee_tool.set_account_id("again_another_id")
+
+        print(employee_tool.get_account_id())
+
+
+if __name__ == "__main__":
+    main()
